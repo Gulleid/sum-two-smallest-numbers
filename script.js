f50e34f949a697d1f3cf2d72b83a0aaf0a40c1ab
Gullied Mohamed <gulleidmohamed93@gmail.com>
2023-06-19 15:26:32 +0300

    sum two smallest number

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // Good Luck 💪🏾
+function sumTwo(numbers) {
+   
+    numbers.sort(function(a, b) {
+      return a - b;
+    });
+  
+    return numbers[0] + numbers[1];
+  }
+  const numbers = [12, 5, 24, 55, 18];
+  const sum = sumTwo(numbers);
+  console.log(sum);
